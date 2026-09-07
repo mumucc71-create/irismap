de3a35d99ad9519e3e5fcebf021101319a122733
Jess <mumucc71@gmail.com>
2026-09-07 22:43:39 +0900

    Restrict legacy web address

--- android/app/src/main/java/com/irismappinglab/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.irismappinglab.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- landing.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  const button=document.querySelector("#menuButton");
-  const navigation=document.querySelector("#mainNavigation");
-  button?.addEventListener("click",()=>{const open=navigation.classList.toggle("open");button.setAttribute("aria-expanded",String(open))});
-  navigation?.addEventListener("click",(event)=>{if(event.target.closest("a")){navigation.classList.remove("open");button?.setAttribute("aria-expanded","false")}});
-})();
